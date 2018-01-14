d2c82687c46d10ff41a58a2fab947a9c507155dd
Danny Lin <tiennanlin@gmail.com>
2018-01-14 20:47:01 +0800

    using Table to show params

--- pages/livehint.js
+++ pages/livehint.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import {Button, LiveHint} from '../hswui';
-import { Card } from 'antd';
+import { Card, Table } from 'antd';
 
 export default class PageLiveHint extends React.Component {
     constructor(props) {
@@ -80,29 +80,75 @@ export default class PageLiveHint extends React.Component {
             effect: "shake"
         });
     }
+
     render() {
+        const apiParamDoc = {
+            columns: [{
+                title: '参数',
+                dataIndex: '0',
+                key: '0',
+            }, {
+                title: '说明',
+                dataIndex: '1',
+                key: '1',
+            }, {
+                title: '类型',
+                dataIndex: '2',
+                key: '2',
+            }, {
+                title: '默认值',
+                dataIndex: '3',
+                key: '3',
+            }],
+            dataSource: [{
+                key: '1',
+                '0': 'message',
+                '1': '展示文字',
+                '2': 'string',
+                '3': '-',
+            }, {
+                key: '1',
+                '0': 'duration',
+                '1': '展示时间(ms)',
+                '2': 'number',
+                '3': '-',
+            }, {
+                key: '1',
+                '0': 'callback',
+                '1': '关闭时调用',
+                '2': 'function',
+                '3': '-',
+            }, {
+                key: '1',
+                '0': 'options',
+                '1': '其他参数',
+                '2': 'object',
+                '3': '-',
+            }],
+        }
         return (
             <div>
                 <h1>LiveHint 消息提示</h1>
                 <p></p>
                 <h2>用法</h2>
-                <Card title="options" >
-                    <h3>status</h3>
+                <Card title="status" >
                     <Button onClick={this.onBtnClick1}>loading</Button>&nbsp;&nbsp;
                     <Button onClick={this.onBtnClick2}>warning</Button>&nbsp;&nbsp;
                     <Button onClick={this.onBtnClick3}>completed</Button>&nbsp;&nbsp;
                     <Button onClick={this.onBtnClick4}>canops</Button>&nbsp;&nbsp;
-                    <br /><br />
-                    <h3>canclose</h3>
+                </Card>
+                <br /><br />
+                <Card title="canclose" >
                     <Button onClick={this.onBtnClick5}>true</Button>&nbsp;&nbsp;
                     <Button onClick={this.onBtnClick6}>false</Button>&nbsp;&nbsp;
-                    <br /><br />
-                    <h3>effect</h3>
+                </Card>
+                <br /><br />
+                <Card title="effect" >
                     <Button onClick={this.onBtnClick7}>shake</Button>&nbsp;&nbsp;
-                    <br /><br />
-                    <h3>append</h3>
+                </Card>
+                <br /><br />
+                <Card title="append" >
                     <Button onClick={this.onBtnClick8}>appendtext</Button>&nbsp;&nbsp;
-                    <br /><br />
                 </Card>
                 <br /><br />
                 <Card title="API">
@@ -119,7 +165,9 @@ LiveHint.show("Hello LiveHint! <a></a>", 3000, null, {
                     </code>
                 </pre>
                 </Card>
-
+                <br /><br />
+                <Table dataSource={apiParamDoc.dataSource} columns={apiParamDoc.columns} pagination={false} />
+                
             </div>
         );
     }
