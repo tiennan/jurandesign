b83a2c477807a6124bc6a7b3f69d0d458e53e6eb
Danny Lin <tiennanlin@gmail.com>
2018-01-15 15:39:54 +0800

    update color and line-height

--- menu.js
+++ menu.js
@@ -25,15 +25,9 @@ export default class Sider extends React.Component {
                     <Menu.Item key="design">设计价值观</Menu.Item>
                     <Menu.Item key="color">色彩</Menu.Item>
                     <Menu.Item key="icon">图标</Menu.Item>
-                    <Menu.Item key="3">主要控件及样式</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>基本控件</span></span>}>
                     <Menu.Item key="button">Button 按钮</Menu.Item>
-                    <Menu.Item key="6">Option 6</Menu.Item>
-                    <SubMenu key="sub3" title="Submenu">
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
                 </SubMenu>
                 <SubMenu key="sub4" title={<span><Icon type="setting" /><span>进阶控件</span></span>}>
                     <Menu.Item key="livehint">LiveHint 消息提示</Menu.Item>
